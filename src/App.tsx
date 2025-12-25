fc1f4cd9b4a6a4b6d62ff77502f72bb3a1d79b61
arthr <arthrmrs@gmail.com>
2025-12-25 15:40:50 -0300

    style: atualizar cor de texto no header do App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import '@/index.css';
 function App() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">Welcome to the Dosed Game</h1>
+      <h1 className="text-4xl font-bold text-accent">Welcome to the Dosed Game</h1>
     </div>
   );
 }
